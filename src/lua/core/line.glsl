de960dd37643c57866cbd2749c483aaea0a3e57e
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2018-05-27 12:22:37 +0200

    some optimizations on vertex lines

--- src/lua/core/line.glsl
+++ src/lua/core/line.glsl
@@ -23,27 +23,38 @@ vec2 at(vec4 bx, vec4 by, float t) {
     return vec2(dot(c_t, bx), dot(c_t, by));
 }
 
-vec2 vertex(int instanceId) {
-    int numInstances = num_curves * segments_per_curve;
-    instanceId = (instanceId + numInstances) % numInstances;
-
-    int curve = instanceId / segments_per_curve;
-    int segment = instanceId % segments_per_curve;
-
-    float curveId = curve / float(max_curves);
-    vec4 bx = texture(curves, vec2(0.0f / float(CURVE_DATA_SIZE), curveId));
-    vec4 by = texture(curves, vec2(1.0f / float(CURVE_DATA_SIZE), curveId));
-
-    float t = segment / float(segments_per_curve);
+vec2 vertex(float curveTY, float t) {
+    vec4 bx = texture(curves, vec2(0.0f / float(CURVE_DATA_SIZE), curveTY));
+    vec4 by = texture(curves, vec2(1.0f / float(CURVE_DATA_SIZE), curveTY));
     return at(bx, by, t);
 }
 
 vec4 position(mat4 transform_projection, vec4 mesh_position) {
     int instanceId = love_InstanceID + int(mesh_position.x);
 
-    vec2 p0 = vertex(instanceId - 1);
-    vec2 p1 = vertex(instanceId);
-    vec2 p2 = vertex(instanceId + 1);
+    int numInstances = num_curves * segments_per_curve;
+
+    ivec3 instanceIds = (ivec3(instanceId) + ivec3(-1, 0, 1) +
+        ivec3(numInstances)) % ivec3(numInstances);
+
+    vec3 curveIds;
+    vec3 ts = modf(vec3(instanceIds) / vec3(segments_per_curve), curveIds);
+
+    vec3 curveTYs = curveIds / vec3(max_curves);
+    vec2 p0, p1, p2;
+
+    if (curveIds == ivec3(curveIds.x)) {
+        vec4 bx = texture(curves, vec2(0.0f / float(CURVE_DATA_SIZE), curveTYs.x));
+        vec4 by = texture(curves, vec2(1.0f / float(CURVE_DATA_SIZE), curveTYs.x));
+
+        p0 = at(bx, by, ts.x);
+        p1 = at(bx, by, ts.y);
+        p2 = at(bx, by, ts.z);
+    } else {
+        p0 = vertex(curveTYs.x, ts.x);
+        p1 = vertex(curveTYs.y, ts.y);
+        p2 = vertex(curveTYs.z, ts.z);
+    }
 
     vec2 d10 = normalize(p1 - p0);
     vec2 d21 = normalize(p2 - p1);
