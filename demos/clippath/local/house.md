2c90b812a897a21ed5d6be80c6e67c4a2cef391c
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2019-03-24 20:40:43 +0100

    added nicer demo svg for clip paths

---
+++ demos/clippath/local/house.md
@@ -0,0 +1,1 @@
+house.svg is from the wonderful http://2dgameartforfree.blogspot.com
--- demos/clippath/main.lua
+++ demos/clippath/main.lua
@@ -7,28 +7,31 @@ require "assets/tovedemo"
 local flow
 
 local function load(svg)
-	-- makes a new graphics, prescaled to 200 px
 	local function newGraphics()
 		return tove.newGraphics(svg, 200)
 	end
 
 	-- just some glue code for presentation.
 	flow = tovedemo.newCoverFlow()
+	-- , "mesh"
 	for _, mode in ipairs {"texture", "mesh"} do
 		local graphics = newGraphics()
 		local quality = {}
 		if mode == "mesh" then
-			quality = {200}
+			quality = {1000}
+			-- work around a current TOVE limitation with uniforms
+			-- and gradient shaders. use flat mesh for rendering.
+			graphics:setUsage("shaders", "avoid")
 		end
 		graphics:setDisplay(mode, unpack(quality))
 		flow:add(mode, graphics)
 	end
 end
 
-load(love.filesystem.read("clippath.svg"))
+load(love.filesystem.read("local/house.svg"))
 
 function love.draw()
-	tovedemo.draw("Clip Paths.")
+	tovedemo.draw("Complex Clip Paths.")
 	flow:draw()
 end
 
